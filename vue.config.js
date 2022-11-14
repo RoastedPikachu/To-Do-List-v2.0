d6860041b00b0a0329911dc399c5102a8c503e60
Boris Karabut <boriskarabytwork@gmail.com>
2022-11-14 10:53:44 +0300

    Добавил файлы конфига

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  /*publicPath: process.env.NODE_ENV === 'production'
+  ? '/To-Do-List-v2.0/'
+  : '/',*/
+  transpileDependencies: true,
+});
